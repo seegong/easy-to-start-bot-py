f8ab544c359d2134500184daf5360a5c5d7d2d68
{seegong} <tw9138ho@gmail.com>
2021-06-10 12:41:54 +0900

    crawler

---
+++ crawler.py
@@ -0,0 +1,29 @@
+import time
+from bs4 import BeautifulSoup
+import requests
+
+import re
+
+
+
+t = time.time()
+
+
+
+fasfd = time.strftime('???? link in time or ~~~', time.localtime(t))
+##or
+sadas =('link')
+
+
+
+
+res = requests.get(fasfd, headers={
+    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36'
+})
+
+soup = BeautifulSoup(res.text, 'html.parser')
+meal = soup.find('?????','?????')
+
+
+
+print (meal)
