37ef7978e04a3263c886c296ea8d6283c58bfa07
{seegong} <tw9138ho@gmail.com>
2021-06-10 12:29:50 +0900

    first commit

---
+++ bot.py
@@ -0,0 +1,35 @@
+import discord
+import asyncio
+import random
+
+import time
+from bs4 import BeautifulSoup
+import requests
+client = discord.Client()
+
+import lib
+
+
+@client.event
+async def on_ready():
+
+    print("----------에러 로그----------")
+    await client.change_presence(activity=discord.Game(name='paly game', type=1))
+
+
+@client.event
+async def on_message(message):
+
+    if message.content.startswith('ping'):
+        await message.channel.send("pong")
+    
+
+    if message.content.startswith('hello'):
+        hi = "hi/hello"
+        hichoice = hi.split("/")
+        hirnmber = random.randint(1, len(hichoice))
+        hiresult = hichoice[hirnmber - 1]
+        await message.channel.send(hiresult)
+
+
+client.run('user token')
